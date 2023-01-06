ee4ede97d9517e7d997b7c2cfb559a65630c0b85
luki1oo1 <choragwicki.lukasz@gmail.com>
2023-01-06 23:15:14 +0100

    select-loading

--- src/app/components/holidays/holidays.component-module.ts
+++ src/app/components/holidays/holidays.component-module.ts
@@ -5,12 +5,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { HolidaysComponent } from './holidays.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  imports: [MatFormFieldModule, MatSelectModule, ReactiveFormsModule, MatOptionModule, CommonModule],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatOptionModule,
+    CommonModule,
+    MatProgressSpinnerModule,
+  ],
   declarations: [HolidaysComponent],
   providers: [],
-  exports: [HolidaysComponent]
+  exports: [HolidaysComponent],
 })
-export class HolidaysComponentModule {
-}
+export class HolidaysComponentModule {}
